87577fc7c1d42d38afc11eb3c945405c81ee76a6
Mauricio Silva <mauricioramonsilvasandoval@gmail.com>
2025-04-24 08:16:07 -0600

    merge

--- lib/game/sprites/trash_bins/metal_bin.dart
+++ lib/game/sprites/trash_bins/metal_bin.dart
@@ -6,17 +6,11 @@ import 'package:flame/components.dart';
 
 class MetalBin extends RecycleBin {
   MetalBin({required super.position})
-    : super(
-        size: Vector2(40, 49),
-        spritePath: 'trash_bins/metal_bin.png',
-      );
+    : super(size: Vector2(40, 49), spritePath: 'trash_bins/metal_bin.png');
 
   @override
   void onCollision(Set<Vector2> intersectionPoints, PositionComponent other) {
-<<<<<<< HEAD
-=======
     print('collision with $other');
->>>>>>> ccaa84c68643bde1c4d9e703bb600912ed0dac39
     if (other is Metal) {
       other.removeFromParent();
       (gameRef as BurgerGame).increaseScore();
